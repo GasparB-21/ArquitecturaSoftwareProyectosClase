6fc0d7e18561748a5025e735d329911ced3ccf70
GasparB-21 <gbarrerassi.inf@upsa.es>
2026-02-12 13:26:08 +0100

    Initial commit - Django project setup

---
+++ Clase_02-12-26/proyecto_clases/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for proyecto_clases project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_clases.settings')
+
+application = get_asgi_application()
